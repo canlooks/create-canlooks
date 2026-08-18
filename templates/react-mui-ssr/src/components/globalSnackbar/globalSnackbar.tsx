081c663e50732e9636bf28c5d209506194d1d78a
C.CanLiang <364021661@qq.com>
2026-08-18 13:18:09 +0800

    1

--- templates/react-mui-ssr/src/components/globalSnackbar/globalSnackbar.tsx
+++ templates/react-mui-ssr/src/components/globalSnackbar/globalSnackbar.tsx
@@ -4,7 +4,7 @@ import {memo, type ComponentProps, type ReactElement, type SyntheticEvent} from 
 
 import {Alert, AlertTitle, Collapse, Grow, Snackbar, type SnackbarCloseReason} from '@mui/material'
 
-import {useSnackbarStore} from '@/stores/snackbar'
+import {useSnackbarStore} from '@/stores/snackbar.store'
 
 import {style} from './globalSnackbar.style'
 
